45ff3211a7c7b01baf66d6ddd207c44a2387c4ff
Lakshay Sood <lksood@ymail.com>
2021-01-30 02:30:58 +0530

    Initial Commit: App is running. Header done

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Text('Hello World'),
+    );
+  }
+}
